853842c87713c439f712e7abd8a9864a7d376ab2
sergeyrudov <255400warr>
2020-11-22 02:06:21 +0200

    - optimized

--- src/main/AbstractExample/androidGetPlatform.java
+++ src/main/AbstractExample/androidGetPlatform.java
@@ -5,7 +5,7 @@ public class androidGetPlatform extends Request {
     @Override
     public String getInfo(Platforms value) {
         if (value.equals(Platforms.ANDROID)) {
-            return "yep, am ANDROID";
+            return "Yes, my platform is: "+ value.toString().toUpperCase();
         }
         return "My OS is NOT: " + value ;
     }
--- src/main/AbstractExample/iosGetPlatform.java
+++ src/main/AbstractExample/iosGetPlatform.java
@@ -5,7 +5,7 @@ public class iosGetPlatform extends Request {
     @Override
     public String getInfo(Platforms value) {
         if (value.equals(Platforms.IOS)) {
-            return "yep, am IOS";
+            return "Yes, my platform is: "+ value.toString().toUpperCase();
         }
         return "My OS is NOT: " + value;
     }
